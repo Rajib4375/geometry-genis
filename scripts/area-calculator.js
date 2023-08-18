498503b68956aa047bd28bebc3f8ae69707c4cd5
rajib4375 <rajibhossain10498@gmail.com>
2023-08-18 11:20:38 +0600

    added the area calculator

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -30,8 +30,57 @@ function calculateRectangleAra(){
     const lengthValueText =lengthFiled.value;
     const length = parseFloat(lengthValueText);
 
+    // validate input width and length
+
+    if(isNaN(width) || isNaN(length)){
+        alert('please insert a number');
+        return;
+    }
+
     const area = width * length;
 
     const rectangleAreaSpan = document.getElementById('rectangle-area');
    rectangleAreaSpan.innerText = area;
+}
+
+// reusable function --> reduce dublicate code
+function calculateParallelogramAra(){
+    const base = getInputValue('parallelogram-base');
+    console.log(base)
+
+    const height = getInputValue('parallelogram-height');
+    console.log(height)
+
+    // validate input
+    if(isNaN(base) || isNaN(height)){
+        alert('please insert a number');
+        return;
+    }
+
+
+    const area = base * height;
+
+    setElementInnerText('parallelogram-area', area);
+
+}
+function calculateEllipseAra(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const mainorRadius = getInputValue('ellipse-mainor-radius');
+    const area = 3.14 * mainorRadius * mainorRadius;
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area', areaTwoDecimal);
+}
+
+// reusable input value filed in number
+function getInputValue(filedId){
+    const inputFiled = document.getElementById(filedId);
+    const inputValueText = inputFiled.value;
+    const value = parseFloat(inputValueText);
+    return value;
+
+}
+
+function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
